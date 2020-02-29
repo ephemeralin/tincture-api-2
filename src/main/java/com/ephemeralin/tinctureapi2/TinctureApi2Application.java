6d59146d8b73a84e8e9c989f8f4cf93219bd3e47
ephemeralin <ephemeralin@gmail.com>
2020-02-29 22:46:55 +0300

    initial commit

---
+++ src/main/java/com/ephemeralin/tinctureapi2/TinctureApi2Application.java
@@ -0,0 +1,13 @@
+package com.ephemeralin.tinctureapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinctureApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinctureApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ephemeralin/tinctureapi2/TinctureApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ephemeralin.tinctureapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinctureApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
