66aaf864da6b631ab754a8308e04c3fe2e4d0528
ephemeralin <ephemeralin@gmail.com>
2020-03-21 18:17:23 +0300

    add feed areas for 'English' mode

--- src/main/java/com/ephemeralin/tincture/model/FeedArea.java
+++ src/main/java/com/ephemeralin/tincture/model/FeedArea.java
@@ -3,5 +3,6 @@ package com.ephemeralin.tincture.model;
 import java.io.Serializable;
 
 public enum FeedArea implements Serializable {
-    dev, tech, life
+    dev, tech, life,
+    dev_eng, tech_eng, life_eng, java_eng
 }
